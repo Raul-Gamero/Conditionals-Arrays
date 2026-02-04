8752fd862b468b4b34955944259bf08618a405fd
Abel <abelstor@gmail.com>
2026-02-04 14:52:35 +0100

    exercise 2

--- app.js
+++ app.js
@@ -27,3 +27,30 @@ function compareWithEquality(a, b) {
 console.log(compareWithEquality(5, "5"));
 console.log(compareWithEquality(10, 10));
 console.log(compareWithEquality(3, 7));
+
+/**
+ * Exercise 2 – Comparison with the Strict Equality Operator (===)
+ * Write a function compareWithStrictEquality(a, b) that:
+ * 
+ * returns "Strictly equal" if a === b
+ * returns "Not strictly equal" otherwise
+ * 👉 Test with:
+ * 
+ * compareWithStrictEquality(5, "5")
+ * compareWithStrictEquality(5, 5)
+ */
+function compareWithStrictEquality(a, b) {
+
+    let comparison;
+
+    if (a === b) {
+        comparison = 'Strictly equal';
+    } else {
+        comparison = 'Not strictly equal';
+    }
+
+    return comparison;
+}
+
+console.log(compareWithStrictEquality(5, "5"));
+console.log(compareWithStrictEquality(5, 5));
